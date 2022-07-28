115683eaaa95c5e44c3d7424db2201fb0e693d31
DoriTran <tranquocdong308@gmail.com>
2022-07-28 09:40:05 +0700

    add Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<div>Hello</div>} />
+        <Route path="*" element={<Navbar/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUserTie } from "@fortawesome/free-solid-svg-icons"
 
 import { useState } from "react"
-import SelectRow from "./Modal/SelectRow"
 
 const Navbar = () => {
     const [language, setLanguage] = useState("English")
@@ -11,8 +10,13 @@ const Navbar = () => {
     return (
         <div className="navbar-container">
             <FontAwesomeIcon className="icon" icon={faUserTie} />
-            <div className="navbar-text">Admin | Language</div>
-            <SelectRow className="navbar-combobox" name="notObject" options={["English", "Tiếng Việt"]} value={language} setInput={setLanguage}/>
+            <div className="navbar-text">Admin | Language:</div>
+            <select className="navbar-combobox" value={language} onChange={event => setLanguage(event.target.value)}>
+                <option value={"English"}>English</option>
+                <option value={"VietNam"}>Tiếng Việt</option>
+            </select>
         </div>
     )
 }
+
+export default Navbar
